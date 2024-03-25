90f23cff286c49b4f5d3c1d63f1eaa66d6f08c83
siddu015 <116783967+siddu015@users.noreply.github.com>
2024-03-25 19:52:30 +0530

    done

--- src/DSA_Java/AVLTree.java
+++ src/DSA_Java/AVLTree.java
@@ -29,12 +29,69 @@ public class AVLTree {
         else
             root.rightChild = insert(root.rightChild, value);
 
-        root.height = Math.max(height(root.leftChild), height(root.rightChild)) + 1;
+        setHeight(root);
+
+        return balance(root);
+    }
+
+    private AVLNode balance(AVLNode root) {
+        if(isLeftHeavy(root)) {
+            if(balanceFactor(root.leftChild) < 0)
+                root.leftChild = rotateLeft(root.leftChild);
+            return rotateRight(root);
+        }
+
+        else if(isRightHeavy(root)) {
+            if(balanceFactor(root.rightChild) > 0)
+                root.rightChild = rotateRight(root.rightChild);
+            return rotateLeft(root);
+        }
 
         return root;
     }
 
+    private AVLNode rotateLeft(AVLNode root) {
+        var newRoot = root.rightChild;
+
+        root.rightChild = newRoot.leftChild;
+        newRoot.leftChild = root;
+
+        setHeight(root);
+        setHeight(newRoot);
+
+        return newRoot;
+    }
+    private AVLNode rotateRight(AVLNode root) {
+        var newRoot = root.leftChild;
+
+        root.leftChild = newRoot.rightChild;
+        newRoot.rightChild = root;
+
+        setHeight(root);
+        setHeight(newRoot);
+
+        return newRoot;
+    }
+
+    private void setHeight(AVLNode node) {
+        node.height = Math.max(
+                height(node.leftChild),
+                height(node.rightChild)) + 1;
+    }
+
     private int height(AVLNode node) {
         return node == null ? -1: node.height;
+    }
+
+    private boolean isLeftHeavy(AVLNode node) {
+        return balanceFactor(node) > 1;
+    }
+
+    private boolean isRightHeavy(AVLNode node) {
+        return balanceFactor(node) < -1;
+    }
+
+    private int balanceFactor(AVLNode node) {
+        return node == null ? 0 : height(node.leftChild) - height(node.rightChild);
     }
 }
--- src/DSA_Java/Main.java
+++ src/DSA_Java/Main.java
@@ -5,21 +5,11 @@ import java.util.Arrays;
 public record Main() {
     public static void main(String[] args) {
          var plant = new AVLTree();
+         plant.insert(30);
          plant.insert(20);
-         plant.insert(2);
-         plant.insert(1);
-         plant.insert(4);
-         plant.insert(29);
-         plant.insert(61);
-         plant.insert(28);
-         plant.insert(5);
-         plant.insert(9);
+         plant.insert(10);
 
-        System.out.println();
-
-
-
+//        System.out.println();
 
     }
 } 
-
