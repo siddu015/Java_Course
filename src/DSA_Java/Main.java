6c43c4d9c8207dec2ceddba344531c6329a0f4b0
siddu015 <116783967+siddu015@users.noreply.github.com>
2024-03-28 23:15:44 +0530

    done

--- src/DSA_Java/Main.java
+++ src/DSA_Java/Main.java
@@ -5,11 +5,14 @@ import java.util.Arrays;
 public record Main() {
     public static void main(String[] args) {
          var plant = new AVLTree();
-         plant.insert(30);
-         plant.insert(20);
-         plant.insert(10);
+         plant.insert(12);
+         plant.insert(3);
+         plant.insert(9);
+         plant.insert(4);
+         plant.insert(6);
+         plant.insert(2);
 
-//        System.out.println();
+        System.out.println();
 
     }
 } 
