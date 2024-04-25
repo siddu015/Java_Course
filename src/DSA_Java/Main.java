fee96180e1b6d4d22b0fcf4e2042c86743b40340
siddu015 <116783967+siddu015@users.noreply.github.com>
2024-04-25 10:55:14 +0530

    done

--- src/DSA_Java/Main.java
+++ src/DSA_Java/Main.java
@@ -1,10 +1,9 @@
 package DSA_Java;
 
-public record Main() {
+public class Main {
     public static void main(String[] args) {
-       int[] array = {1, 2, 4 ,5, 6, 7};
-        var ass = new Search();
-        System.out.println(ass.ternarySearchRec(array, 5));
+        int[] array = {};
+        var search = new Search();
+        System.out.println(search.jumpSearch(array, 3));
     }
-
 } 
--- src/DSA_Java/Search.java
+++ src/DSA_Java/Search.java
@@ -70,4 +70,34 @@ public class Search {
         return ternarySearchRec(array, target, mid1 + 1, mid2 - 1);
     }
 
+    public int jumpSearch(int[] array, int target) {
+        var blockSize = (int) Math.sqrt(array.length);
+        var start = 0;
+        var next = blockSize;
+
+        while (start < array.length && array[next - 1] < target) {
+            start = next;
+            next += blockSize;
+            if(next > array.length)
+                next = array.length;
+        }
+
+        for(int i = start; i < next; i++)
+            if(array[i] == target)
+                return i;
+
+        return -1;
+    }
+
+    public int exponentialSearch(int[] array, int target) {
+        int bound = 1;
+        while(bound < array.length && array[bound] < target)
+            bound *= 2;
+
+        var left = bound/2;
+        var right = Math.min(bound, array.length -1);
+
+        return binarySearchRec(array, target, left, right);
+    }
+
 }
