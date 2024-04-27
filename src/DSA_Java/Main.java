8d977fe0b6b994116dc6cdf5f78354cae7240f61
siddu015 <116783967+siddu015@users.noreply.github.com>
2024-04-27 23:09:33 +0530

    done

--- src/DSA_Java/Main.java
+++ src/DSA_Java/Main.java
@@ -2,8 +2,7 @@ package DSA_Java;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = {};
-        var search = new Search();
-        System.out.println(search.jumpSearch(array, 3));
+        var text = new StringUtils();
+        System.out.println(text.capitalize("    hello world             hi it is" ));
     }
 } 
---
+++ src/DSA_Java/StringUtils.java
@@ -0,0 +1,88 @@
+package DSA_Java;
+
+import java.util.*;
+
+public class StringUtils {
+    public int countVowels(String str) {
+        if(str == null) return 0;
+
+        Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+        return (int) str.chars().mapToObj(c -> (char) c).filter(vowels::contains).count();
+    }
+
+    public String reverse(String str) {
+        if(str == null) return "";
+
+        StringBuilder reversed = new StringBuilder();
+        for(var c: str.toCharArray())
+            reversed.insert(0, c);
+        return reversed.toString();
+    }
+
+    public String reverseWords(String sentence) {
+        if(sentence == null) return "";
+
+        String[] words = sentence.split(" ");
+        Collections.reverse(Arrays.asList(words));
+        return String.join(" ", words);
+    }
+
+    public boolean rotation(String str1, String str2) {
+        if(str1 == null || str2 == null) return false;
+
+        return str1.length() == str2.length()
+                && (str1 + str1).contains(str2);
+    }
+
+    public String removeDuplicates(String str) {
+        if(str == null) return "";
+
+        Set<Character> seen = new HashSet<>();
+
+        StringBuilder result = new StringBuilder();
+        for(var c: str.toCharArray())
+            if(!seen.contains(c)) {
+                result.append(c);
+                seen.add(c);
+            }
+
+        return result.toString();
+    }
+
+    public Character getMaxOccurringChar(String str) {
+        if(str == null || str.isEmpty())
+            throw new IllegalArgumentException();
+
+        final int ASCII_SIZE = 256;
+        int[] frequencies = new int[ASCII_SIZE];
+        for(var c: str.toCharArray())
+            frequencies[c]++;
+
+        int max = 0;
+        char result = ' ';
+        for(var i = 0; i < frequencies.length; i++)
+            if(frequencies[i] > max) {
+                max = frequencies[i];
+                result = (char) i;
+            }
+
+        return result;
+    }
+
+    public String capitalize(String sentence) {
+        if(sentence == null || sentence.trim().isEmpty())
+            return "";
+
+        String[] words = sentence
+                            .trim()
+                            .replaceAll(" +"," ")
+                            .split(" ");
+
+        for(var i = 0; i < words.length; i++)
+            words[i] =
+                    words[i].substring(0, 1).toUpperCase() +
+                    words[i].substring(1).toLowerCase();
+
+        return String.join(" ", words);
+    }
+}
